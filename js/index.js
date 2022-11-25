d2e94be32ea22e27b729005cfd2975973023c0a7
Amivian <amiviann@gmail.com>
2022-11-25 14:20:11 +0100

    paystack

--- js/index.js
+++ js/index.js
@@ -1,12 +1,24 @@
+const paymentForm = document.getElementById('orderForm');
+paymentForm.addEventListener("submit", payWithPaystack, false);
 
+function payWithPaystack(e) {
+e.preventDefault();
 
+let handler = PaystackPop.setup({
+    key: 'pk_live_72b6f671a4f806a06600572b1a4c95506bb0d87d', // Replace with your public key
+    email: document.getElementById("email-address").value,
+    amount: document.getElementById("amount").text * 100,
+    currency: 'NGN',
+    // ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function(){
+    alert('Window closed.');
+    },
+    callback: function(response){
+    let message = 'Payment complete! Reference: ' + response.reference;
+    alert(message);
+    }
+});
 
-// if("serviceWorker" in navigator){
-//     navigator.serviceWorker.register("../service_worker.js").then(registration=>{
-//       console.log("SW Registered!");
-//     }).catch(error=>{
-//       console.log("SW Registration Failed");
-//     });
-// }else{
-//   console.log("Not supported");
-// }
+handler.openIframe();
+}
