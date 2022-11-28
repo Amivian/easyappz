4b6076e4926b80e6c0f9071738980a0e8c8b8ee1
Amivian <amiviann@gmail.com>
2022-11-28 13:22:25 +0100

    added

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 const paymentForm = document.getElementById('orderForm');
 paymentForm.addEventListener("submit", payWithPaystack, false);
 
+var price = document.getElementById("amount").innerHTML * 100;
+
 function payWithPaystack(e) {
 e.preventDefault();
 
 let handler = PaystackPop.setup({
     key: 'pk_live_72b6f671a4f806a06600572b1a4c95506bb0d87d', // Replace with your public key
     email : document.getElementById("email-address").value,
-    amount : document.getElementById("amount").innerHTML * 100,
+    amount : price,
     currency: 'NGN',
     // ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
     // label: "Optional string that replaces customer email"
