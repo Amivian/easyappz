37c6fbd60a3afd301cc8d117b4f0e4c51218c866
Amivian <amiviann@gmail.com>
2022-11-28 11:56:38 +0100

    changes

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ e.preventDefault();
 let handler = PaystackPop.setup({
     key: 'pk_live_72b6f671a4f806a06600572b1a4c95506bb0d87d', // Replace with your public key
     email: document.getElementById("email-address").value,
-    amount: 1000,
+    amount: 1000 * 100,
     currency: 'NGN',
     // ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
     // label: "Optional string that replaces customer email"
